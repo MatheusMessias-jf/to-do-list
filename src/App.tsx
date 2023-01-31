7815860a79391aca42c4565e0d6a85889037e6f9
Matheus Messias <matheus.jf@live.com>
2023-01-30 22:40:29 -0300

    Styling main page

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,23 @@ export function App() {
         <img src={toDoLogo} alt="" />
       </div>
       <div className={styles.task_input}>
-        <input type="text" />
-        <button>Criar</button>
+        <input type="text" placeholder="Adicione uma nova tarefa" />
+        <button>
+          Criar<span className="material-symbols-outlined">add_circle</span>
+        </button>
+      </div>
+      <div className={styles.task_list_content}>
+        <div className={styles.tasks_header}>
+          <div className={styles.created_tasks}>
+            <p>Tarefas criadas</p>
+            <div className={styles.created_tasks_counter}>0</div>
+          </div>
+          <div className={styles.completed_tasks}>
+            <p>Tarefas Concluídas</p>
+            <div className={styles.completed_tasks_counter}>0</div>
+          </div>
+        </div>
+        <div className={styles.task_list}></div>
       </div>
     </div>
   );
