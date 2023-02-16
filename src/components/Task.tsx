f24da536f22c2b525ba217fafda1868401559f36
Matheus Messias <matheus.jf@live.com>
2023-02-15 22:46:35 -0300

    Using conditional expression to style checkbox

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -2,11 +2,23 @@ import styles from "./Task.module.css";
 import { Trash } from "phosphor-react";
 import * as Checkbox from "@radix-ui/react-checkbox";
 import { CheckIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 
 export function Task() {
+  const [isChecked, setIsChecked] = useState(false);
+  const handleIsChecked = (checked: any) => {
+    setIsChecked(checked);
+  };
+
   return (
     <div className={styles.task}>
-      <Checkbox.Root className={styles.CheckboxRoot} defaultChecked id="c1">
+      <Checkbox.Root
+        onCheckedChange={handleIsChecked}
+        className={
+          isChecked ? styles.CheckboxRoot : styles.CheckboxRootNotChecked
+        }
+        id="c1"
+      >
         <Checkbox.Indicator className={styles.CheckboxIndicator}>
           <CheckIcon />
         </Checkbox.Indicator>
